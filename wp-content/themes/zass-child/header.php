150d98d5310d179f5f3f68ba36f4b8981ed71d2a
Saeed Soltani <soltoon.theleader@gmail.com>
2021-07-19 13:11:39 +0430

    fix main document landmark

--- wp-content/themes/zass-child/header.php
+++ wp-content/themes/zass-child/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<html role="document" <?php language_attributes(); ?>>
 
 <head>
   <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
