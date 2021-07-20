9673ab78766d24be7658d1bc4b66966efa8a91f0
Saeed Soltani <soltoon.theleader@gmail.com>
2021-07-20 14:15:47 +0430

    fix sidebar landmark

--- wp-content/themes/zass-child/sidebar-offcanvas.php
+++ wp-content/themes/zass-child/sidebar-offcanvas.php
@@ -4,10 +4,10 @@ $zass_sidebar_choice = apply_filters('zass_has_offcanvas_sidebar', '');
 ?>
 
 <?php if (function_exists('dynamic_sidebar') && $zass_sidebar_choice != 'none' && is_active_sidebar($zass_sidebar_choice)) : ?>
-  <div class="sidebar off-canvas-sidebar">
+  <aside class="sidebar off-canvas-sidebar" aria-label="Product Categories Sidebar">
     <a class="close-off-canvas" alt="close" title="close" href="#"></a>
     <div class="off-canvas-wrapper">
       <?php dynamic_sidebar($zass_sidebar_choice); ?>
     </div>
-  </div>
+  </aside>
 <?php endif; ?>
